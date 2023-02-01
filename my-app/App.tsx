dfa1e07ac799ec0b3d4cfe9979455b96025a2bda
gusmarquesDev <gus.desenvolvedor@hotmail.com>
2023-02-01 03:39:16 -0300

    feat: created page Create Account

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/pages/Login/Login'
-
+import CreateAccount from './src/pages/CreateAccount/CreateAccount';
 
 export default function App() {
   return (
-      <Login/>
+      <CreateAccount/>
   );
 }
 
---
+++ my-app/src/pages/CreateAccount/CreateAccount.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { FullHeightScrollView, InputWraper } from '.././Login/Login.styles'
+import { SafeAreaKeyboardContainer, SafeAreaContainer } from '../../utils/screen/SafeArea'
+import { Content } from '../../utils/screen/Page'
+import { Input } from '../../components/Input/Input'
+import { Button } from '../../components/Button/Button'
+
+export type CreateAccount = {
+
+}
+
+
+const CreateAccount: React.FC<CreateAccount> = ({ }) => {
+
+
+   return (
+      <>
+         <SafeAreaKeyboardContainer>
+            <SafeAreaContainer>
+               <FullHeightScrollView>
+                  <Content>
+                     <InputWraper>
+                        <Input
+                           label='First Name'
+                           value={'value'}
+                        />
+                     </InputWraper>
+                     <InputWraper>
+                        <Input
+                           label='Last Name'
+                           value={'value'}
+                        />
+                     </InputWraper>
+                     <InputWraper>
+                        <Input
+                           label='Password'
+                           value={'value'}
+                        />
+                     </InputWraper>
+                  </Content>
+                  <Content>
+                  <Button/>
+                  </Content>
+               </FullHeightScrollView>
+            </SafeAreaContainer>
+         </SafeAreaKeyboardContainer>
+      </>
+   )
+}
+
+export default CreateAccount
